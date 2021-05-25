42a765326bc25a5abcd04c4a7138b88ac60ae11d
qgan7125 <35419958+qgan7125@users.noreply.github.com>
2021-05-25 12:45:41 -0700

    add more fields in the table and enable

--- UI_Preview/script.js
+++ UI_Preview/script.js
@@ -1,37 +1,36 @@
-    //change display context according to tabs
-    function openTab(pid) {
-        var data = document.getElementById("data").children;
-        for (var i = 0; i < data.length; i++) {
-            data[i].style.display = "none";
-        }
-        var actContent = document.getElementById(pid);
-        actContent.style.display = "block";
-    }
-    //show specified identifier record
-    async function showRawRecord(id) {
-        var url = `https://mars.cyverse.org/thing/${id}?full=false`;
-        console.log(url);
-        fetch(url)
-            .then(response => response.json())
-            .then(doc => {
-                var e = document.getElementById("object_show");
-                e.innerHTML = prettyPrintJson.toHtml(doc, FormatOptions = {
-                    indent: 2
-                });
-            })
-    }
+//show specified identifier record
+async function showRawRecord(id) {
+    var url = `https://mars.cyverse.org/thing/${id}?full=false`;
+    console.log(url);
+    fetch(url)
+        .then(response => response.json())
+        .then(doc => {
+            var e = document.getElementById("records_show");
+            e.innerHTML = prettyPrintJson.toHtml(doc, FormatOptions = {
+                indent: 2
+            });
+        })
+}
 
-    //select row and show record informaton
-    function rowClick(e, row) {
-        console.log(row._row);
-        var id = row._row.data.id;
-        showRawRecord(id);
-    }
+//select row and show record informaton
+function rowClick(e, row) {
+    console.log(row._row);
+    var id = row._row.data.id;
+    showRawRecord(id);
+}
 
 
-    //filter the records accordig users' input
-    function doIdFilter(eid) {
-        var e = document.getElementById(eid).value;
-        console.log("Filter ID: " + e);
-        table.setFilter("id", "like", e);
-    }
+//filter the records accordig users' input
+function doIdFilter(eid) {
+    var e = document.getElementById(eid).value;
+    console.log("Filter ID: " + e);
+    table.setFilter("id", "like", e);
+}
+
+//update load data
+function Dataloaded(url, params, response) {
+    var e = document.getElementById("total_records");
+    console.log(response);
+    e.innerHTML = response.data.length;
+    return response;
+}
