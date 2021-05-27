bca08cb700edd3119b001047a459bb787bf16250
qgan7125 <35419958+qgan7125@users.noreply.github.com>
2021-05-27 09:22:46 -0700

    move all script into js file

--- UI_Preview/script.js
+++ UI_Preview/script.js
@@ -36,3 +36,62 @@ function Dataloaded(url, params, response) {
     e2.innerHTML = response.total_records;
     return response;
 }
+
+function tableAndSplit() {
+    //load records from server into tabulator
+    var table = new Tabulator("#records_table", {
+        layout: "fitColumns",
+        height: 300,
+        placeholder: "No data availble",
+        ajaxURL: "https://mars.cyverse.org/thing/?offset=0&limit=1000&status=200",
+        paginationDataSent: {
+            "page": "offset",
+            "size": "limit",
+        },
+        ajaxURLGenerator: function(url, config, params) {
+            return url + `?offset=${params.offset}&limit=${params.limit}&status=200`;
+        },
+        ajaxProgressiveLoad: "scroll",
+        paginationSize: 1000,
+        ajaxParams: {
+            key1: "id",
+            keys: "tcreated"
+        },
+        columns: [{
+            "title": "id",
+            field: "id",
+        }, {
+            "title": "authority_id",
+            field: "authority_id",
+            width: 150
+        }, {
+            "title": "time created",
+            field: "tcreated",
+            width: 300
+        }, {
+            "title": "status",
+            field: "resolved_status",
+            width: 150
+        }, {
+            "title": "url",
+            field: "resolved_url"
+        }, {
+            "title": "elapsed",
+            field: "resolve_elapsed"
+        }],
+        ajaxResponse: Dataloaded,
+        rowClick: rowClick,
+        ajaxSorting: true,
+        selectable: 1
+    });
+
+    //add split bar horizonal and vertical
+    Split(["#records_table", "#bottom"], {
+        gutterSize: 30,
+        direction: "vertical",
+        sizes: [30, 70],
+    });
+    Split(["#left", "#right"], {
+        gutterSize: 10,
+    });
+}
