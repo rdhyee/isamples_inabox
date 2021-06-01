1ffee4d55b352a07962ad95ca9c188237bdb665a
qgan7125 <35419958+qgan7125@users.noreply.github.com>
2021-05-31 19:17:10 -0700

    UI 2.0

--- UI_Preview/Records_Script.js
+++ UI_Preview/Records_Script.js
@@ -1,3 +1,37 @@
+//create split bar for table and content
+Split(["#records_table", "#info"], {
+    direction: "vertical",
+    sizes: [30, 70],
+    gutterSize: 30,
+});
+
+var infoSplit = Split(["#InfoLeft", "#InfoRight"], {
+    gutterSize: 10,
+    minSize: [200, 200],
+})
+
+//change the raw data and iSample data panels directions
+function changePanes(direction) {
+    var container = document.getElementsByClassName("infoContainer")[0];
+    if (direction == 'h') {
+        container.style.flexDirection = "row";
+        infoSplit.destroy();
+        infoSplit = Split(["#InfoLeft", "#InfoRight"], {
+            gutterSize: 10,
+            minSize: [200, 200],
+        })
+    } else {
+        container.style.flexDirection = "column";
+        infoSplit.destroy();
+        infoSplit = Split(["#InfoLeft", "#InfoRight"], {
+            gutterSize: 10,
+            minSize: [100, 100],
+            direction: "vertical",
+        })
+    }
+}
+
+//table ---------------------------------------------------------------------
 //show specified identifier record
 async function showRawRecord(id) {
     var url = `https://mars.cyverse.org/thing/${id}?full=false`;
@@ -19,9 +53,7 @@ async function showTypes() {
         .then(res => res.json())
         .then(types => {
             var e = document.getElementById("types_show");
-            if (e) {
-                e.remove();
-            }
+            if (e) e.remove();
             typeTable(types);
         });
 }
@@ -30,22 +62,30 @@ async function showTypes() {
 function typeTable(data) {
     var container = document.getElementById("types_table");
     var table = document.createElement("table");
+    var caption = table.createCaption();
+    caption.innerHTML = "<h3>Types</h3>";
     table.id = "types_show";
-    var rowName = table.insertRow();
-    var name = rowName.insertCell();
-    name.innerHTML = "Name";
-    name.scope = "header";
-    for (var i = 0; i < data.length; i++) {
+    var i = 0;
+    var rowName;
+    var name;
+    var rowCount;
+    var count;
+    while (i < data.length) {
+        if (i % (data.length / 4 | 0) == 0) {
+            rowName = table.insertRow();
+            name = rowName.insertCell();
+            rowCount = table.insertRow();
+            count = rowCount.insertCell();
+            name.innerHTML = "Name";
+            name.scope = "header";
+            count.innerHTML = "Count";
+            count.scope = "header";
+        }
         var eName = rowName.insertCell();
         eName.innerHTML = data[i].item_type;
-    }
-    var rowCount = table.insertRow();
-    var count = rowCount.insertCell();
-    count.innerHTML = "Count";
-    count.scope = "header";
-    for (var j = 0; j < data.length; j++) {
         var eCount = rowCount.insertCell();
-        eCount.innerHTML = data[j].count;
+        eCount.innerHTML = data[i].count;
+        i++;
     }
     container.appendChild(table);
 }
@@ -57,7 +97,6 @@ function rowClick(e, row) {
     showRawRecord(id);
 }
 
-
 //filter the records accordig users' input
 function doIdFilter(eid) {
     var e = document.getElementById(eid).value;
@@ -67,33 +106,29 @@ function doIdFilter(eid) {
 
 //update load data
 function Dataloaded(url, params, response) {
-    var e1 = document.getElementById("records_load");
+    var e1 = document.getElementById("records_loaded");
     var e2 = document.getElementById("total_records");
     console.log(response);
     e1.innerHTML = parseInt(e1.innerHTML) + response.data.length;
     e2.innerHTML = response.total_records;
-    showTypes();
-    var a = table.getAjaxUrl();
-    console.log(a);
+    showTypes()
     return response;
 }
+
 //load records from server into tabulator
 var table = new Tabulator("#records_table", {
     layout: "fitColumns",
-    height: 300,
     placeholder: "No data availble",
-    ajaxURL: "https://mars.cyverse.org/thing/",
+    ajaxURL: "https://mars.cyverse.org/thing/?offset=0&limit=1000&status=200",
     paginationDataSent: {
         "page": "offset",
         "size": "limit",
     },
 
-    ajaxURLGenerator: function(url, config, params) {
-        return url + `?offset=${params.offset}&limit=${params.limit}&status=200`;
-    },
     ajaxRequesting: function(url, params) {
         console.log(url);
         console.log(params);
+        return `https://mars.cyverse.org/thing/?offset=${params.offset}&limit=${params.limit}&status=200`;
     },
     ajaxProgressiveLoad: "scroll",
     paginationSize: 1000,
@@ -129,30 +164,22 @@ var table = new Tabulator("#records_table", {
     rowClick: rowClick,
     selectable: 1
 });
+//table ---------------------------------------------------------------------
 
-//add split bar horizonal and vertical
-Split(["#records_table", "#bottom"], {
-    gutterSize: 30,
-    direction: "vertical",
-    sizes: [30, 70],
-});
-Split(["#left", "#right"], {
-    gutterSize: 10,
-});
 
 //enable input submit when users press enter
 var inputText = document.getElementById("id_filter");
 inputText.addEventListener("keyup", function(event) {
     if (event.keyCode === 13) {
         event.preventDefault();
-        document.getElementById("bt_fillter").click();
+        document.getElementById("bt_filter").click();
     }
 })
 
 //add types in a popup box
-var modal = document.getElementById("types");
+var modal = document.getElementsByClassName("types")[0];
 var bt_types = document.getElementById("bt_types");
-var closeBt = document.getElementById("close");
+var closeBt = document.getElementsByClassName("closeBt")[0];
 
 bt_types.addEventListener('click', openTypes);
 closeBt.addEventListener('click', closeTypes);
