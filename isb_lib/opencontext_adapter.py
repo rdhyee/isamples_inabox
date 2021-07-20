3ef321f24f0419062d149bcb96edbdb9b2965fde
Danny Mandel <danny.mandel@gmail.com>
2021-07-20 15:23:44 -0700

    Opencontext things (#34)
    * https://github.com/isamplesorg/isamples_inabox/issues/32
    
    Checkpoint -- can run and get the first batch of records
    
    * Checkpoint
    
    * Checkpoint, can load records into the db
    
    * Checkpoint, things are working
    
    * Checkpoint, a few fixes
    
    * A bit of cleanup
    
    * Debug logs for timing consideration
    
    * Handle the boundary condition properly

---
+++ isb_lib/opencontext_adapter.py
@@ -0,0 +1,176 @@
+import datetime
+import time
+
+import isb_lib.core
+import logging
+import requests
+import igsn_lib.models.thing
+import typing
+import dateparser
+
+HTTP_TIMEOUT = 10.0  # seconds
+OPENCONTEXT_API = "https://opencontext.org/subjects-search/.json?add-attribute-uris=1&attributes=obo-foodon-00001303%2Coc-zoo-has-anat-id%2Ccidoc-crm-p2-has-type%2Ccidoc-crm-p45-consists-of%2Ccidoc-crm-p49i-is-former-or-current-keeper-of%2Ccidoc-crm-p55-has-current-location%2Cdc-terms-temporal%2Cdc-terms-creator%2Cdc-terms-contributor&prop=oc-gen-cat-sample-col%7C%7Coc-gen-cat-bio-subj-ecofact%7C%7Coc-gen-cat-object&response=metadata%2Curi-meta&sort=updated--desc"
+MEDIA_JSON = "application/json"
+
+
+def get_logger():
+    return logging.getLogger("isb_lib.opencontext_adapter")
+
+
+class OpenContextItem(object):
+    AUTHORITY_ID = "OPENCONTEXT"
+
+    def __init__(self, identifier: str, source):
+        self.identifier = identifier
+        self.item = source
+
+    def as_thing(
+        self,
+        t_created: datetime.datetime,
+        status: int,
+        resolved_url: str,
+        t_resolved: datetime.datetime,
+        resolve_elapsed: float,
+        media_type: str = None,
+    ) -> igsn_lib.models.thing.Thing:
+        L = get_logger()
+        L.debug("OpenContextItem.asThing")
+        if media_type is None:
+            media_type = MEDIA_JSON
+        _thing = igsn_lib.models.thing.Thing(
+            id=self.identifier,
+            tcreated=t_created,
+            item_type=None,
+            authority_id=OpenContextItem.AUTHORITY_ID,
+            resolved_url=resolved_url,
+            resolved_status=status,
+            tresolved=t_resolved,
+            resolve_elapsed=resolve_elapsed,
+        )
+        if not isinstance(self.item, dict):
+            L.error("Item is not an object")
+            return _thing
+        _thing.item_type = "sample"
+        _thing.related = None
+        _thing.resolved_media_type = media_type
+        # Note that we can't use this field in opencontext as the information is batched and we don't have access
+        # to the raw http response here.
+        # _thing.resolve_elapsed = resolve_elapsed
+        _thing.resolved_content = self.item
+        return _thing
+
+
+class OpenContextRecordIterator(isb_lib.core.IdentifierIterator):
+    def __init__(
+        self,
+        offset: int = 0,
+        max_entries: int = -1,
+        date_start: datetime.datetime = None,
+        date_end: datetime.datetime = None,
+        page_size: int = 100,
+    ):
+        super().__init__(
+            offset=offset,
+            max_entries=max_entries,
+            date_start=date_start,
+            date_end=date_end,
+            page_size=page_size,
+        )
+        self.url = OPENCONTEXT_API
+
+    def records_in_page(self):
+        L = get_logger()
+        L.debug("records_in_page")
+        headers = {"Accept": "application/json", "user-agent": "isamplesbot-3000/0.0.1"}
+        _page_size = self._page_size
+        params = {
+            "rows": _page_size,
+        }
+        more_work = True
+        num_records = 0
+        while more_work:
+            L.info("trying to hit %s", self.url)
+            response = requests.get(
+                self.url, params=params, headers=headers, timeout=HTTP_TIMEOUT
+            )
+
+            if response.status_code != 200:
+                L.error(
+                    "Unable to load records; status: %s; reason: %s",
+                    response.status_code,
+                    response.reason,
+                )
+                break
+            # L.debug("recordsInProject data: %s", response.text[:256])
+            data = response.json()
+            for record in data.get("oc-api:has-results", {}):
+                L.info("records_in_page Record id: %s", record.get("uri", None))
+                # print(json.dumps(record, indent=2))
+                # raise NotImplementedError
+                yield record
+                num_records += 1
+            self.url = data.get("next-json")
+            if len(data.get("oc-api:has-results", {})) < _page_size:
+                more_work = False
+            elif self.url is None:
+                # If we hit the last page, there won't be a 'next-json' key
+                more_work = False
+            elif 0 < self._max_entries <= num_records:
+                more_work = False
+            elif num_records == self._page_size:
+                more_work = False
+
+    def loadEntries(self):
+        self._cpage = []
+        self._page_offset = 0
+        counter = 0
+        for item in self.records_in_page():
+            self._cpage.append(item)
+            counter += 1
+            if 0 < self._max_entries < counter:
+                break
+        self._total_records = len(self._cpage)
+
+    def _getPage(self):
+        """Override this method to retrieve a page of entries from the service.
+
+        After completion, self._cpage contains the next page of entries or None if there
+        are no more pages available, and self._page_offset is set to the first entry (usually 0)
+
+        """
+        is_none = self._cpage is None
+        if is_none or self._page_offset >= len(self._cpage):
+            self.loadEntries()
+        if self._coffset >= self._total_records:
+            return
+        self._page_offset = 0
+
+    def last_url_str(self) -> typing.AnyStr:
+        return self.url
+
+
+def load_thing(
+    thing_dict: typing.Dict, t_resolved: datetime.datetime, url: typing.AnyStr
+) -> igsn_lib.models.thing.Thing:
+    """
+    Load a thing from its source.
+
+    Minimal parsing of the thing is performed to populate the database record.
+
+    Args:
+        thing_dict: Dictionary representing the thing
+        t_resolved: When the item was resolved from the source
+
+    Returns:
+        Instance of Thing
+    """
+    L = get_logger()
+    id = thing_dict["uri"]
+    t_created = dateparser.parse(thing_dict.get("published"))
+    L.info("loadThing: %s", id)
+    item = OpenContextItem(id, thing_dict)
+    # TODO, unlike the other collections, we are fetching these via a pre-paginated API, so we can't put anything in the
+    # db if we fail looking up an identifier because we won't have an identifier if the lookup fails.  Maybe there is
+    # nothing to do here, but it's a difference between collections.
+    thing = item.as_thing(t_created, 200, url, t_resolved, None)
+    return thing
---
+++ scripts/opencontext_things.py
@@ -0,0 +1,157 @@
+import datetime
+import logging
+import click
+import click_config_file
+import asyncio
+import isb_lib.core
+import isb_lib.opencontext_adapter
+import igsn_lib.models
+import concurrent.futures
+import heartrate
+import sqlalchemy
+import sqlalchemy.orm
+import sqlalchemy.exc
+
+LOG_LEVELS = {
+    "DEBUG": logging.DEBUG,
+    "INFO": logging.INFO,
+    "WARNING": logging.WARNING,
+    "WARN": logging.WARNING,
+    "ERROR": logging.ERROR,
+    "FATAL": logging.CRITICAL,
+    "CRITICAL": logging.CRITICAL,
+}
+LOG_DATE_FORMAT = "%Y-%m-%dT%H:%M:%S"
+LOG_FORMAT = "%(asctime)s %(name)s:%(levelname)s: %(message)s"
+BACKLOG_SIZE = 40
+
+
+def get_logger():
+    return logging.getLogger("main")
+
+
+def wrap_load_thing(thing_dict, tc):
+    """Return request information to assist future management"""
+    try:
+        return tc, isb_lib.opencontext_adapter.load_thing(thing_dict)
+    except:
+        pass
+    return tc, None
+
+
+async def _load_open_context_entries(session, max_count, start_from):
+    L = get_logger()
+    records = isb_lib.opencontext_adapter.OpenContextRecordIterator(
+        max_entries=max_count, date_start=start_from, page_size=200
+    )
+
+    num_ids = 0
+    for record in records:
+        L.info("got next id from open context %s", record)
+        num_ids += 1
+        id = record["uri"]
+        try:
+            res = session.query(igsn_lib.models.thing.Thing.id).filter_by(id=id).one()
+            logging.info("Already have %s", id)
+        except sqlalchemy.orm.exc.NoResultFound:
+            logging.debug("Don't have %s", id)
+            thing = isb_lib.opencontext_adapter.load_thing(
+                record, datetime.datetime.now(), records.last_url_str()
+            )
+            try:
+                logging.debug("Going to add thing to session")
+                session.add(thing)
+                logging.debug("Added thing to session")
+                session.commit()
+                logging.debug("committed session")
+            except sqlalchemy.exc.IntegrityError as e:
+                session.rollback()
+                logging.error("Item already exists: %s", record)
+
+    L.info("total num records %d", num_ids)
+
+    # total_requested = 0
+    # total_completed = 0
+    # more_work = True
+    # num_prepared = BACKLOG_SIZE  # Number of jobs to prepare for execution
+    # with concurrent.futures.ThreadPoolExecutor(
+    #     max_workers=1
+    # ) as executor:
+    #     while more_work:
+    #         while (
+    #             len(futures) < BACKLOG_SIZE
+    #             and total_requested < max_count
+    #             and num_prepared > 0
+    #         ):
+    #             record = next(records)
+    #             print("got next id from open context %s", record)
+
+
+def load_open_context_entries(session, max_count, start_from=None):
+    loop = asyncio.get_event_loop()
+    future = asyncio.ensure_future(
+        _load_open_context_entries(session, max_count, start_from=start_from)
+    )
+    loop.run_until_complete(future)
+
+
+def getDBSession(db_url):
+    engine = igsn_lib.models.getEngine(db_url)
+    igsn_lib.models.createAll(engine)
+    session = igsn_lib.models.getSession(engine)
+    return session
+
+
+@click.group()
+@click.option(
+    "-d", "--db_url", default=None, help="SQLAlchemy database URL for storage"
+)
+@click.option(
+    "-v",
+    "--verbosity",
+    default="DEBUG",
+    help="Specify logging level",
+    show_default=True,
+)
+@click.option(
+    "-H", "--heart_rate", is_flag=True, help="Show heartrate diagnositcs on 9999"
+)
+@click_config_file.configuration_option(config_file_name="opencontext.cfg")
+@click.pass_context
+def main(ctx, db_url, verbosity, heart_rate):
+    ctx.ensure_object(dict)
+    verbosity = verbosity.upper()
+    logging.basicConfig(
+        level=LOG_LEVELS.get(verbosity, logging.DEBUG),
+        format=LOG_FORMAT,
+        datefmt=LOG_DATE_FORMAT,
+    )
+    L = get_logger()
+    if verbosity not in LOG_LEVELS.keys():
+        L.warning("%s is not a log level, set to INFO", verbosity)
+
+    L.info("Using database at: %s", db_url)
+    ctx.obj["db_url"] = db_url
+    if heart_rate:
+        heartrate.trace(browser=True)
+
+
+@main.command("load")
+@click.option(
+    "-m",
+    "--max_records",
+    type=int,
+    default=1000,
+    help="Maximum records to load, -1 for all",
+)
+@click.pass_context
+def load_records(ctx, max_records):
+    L = get_logger()
+    session = getDBSession(ctx.obj["db_url"])
+    L.info("loadRecords: %s", str(session))
+    # ctx.obj["db_url"] = db_url
+    load_open_context_entries(session, max_records, None)
+
+
+if __name__ == "__main__":
+    main()
