f6be025623141ec6e54814c2f95f971c2360f877
Danny Mandel <danny.mandel@gmail.com>
2021-08-31 13:48:45 -0700

    Get tests to run as part of github (#59)
    * Get tests to run as part of github
    
    * Update poetry.lock to run tests on github
    
    * Mark expected failures, let things compile
    
    * Don't try to hit live solr server by default

--- tests/test_smithsonian.py
+++ tests/test_smithsonian.py
@@ -1,5 +1,6 @@
 from isb_lib import smithsonian_adapter
 
+
 def test_normalized_identifier():
     raw_id = "https://n2t.net/foobarbaz"
     normalized_id = smithsonian_adapter._normalized_id(raw_id)
