4d67aa3904b454055ed5410250ad643c97e74d92
hyunjusong <thdguswn989@gmail.com>
2022-07-15 11:40:08 -0700

    add print full hierarchy script, fixes for flake

--- scripts/taxonomy/create_label_hierarchy.py
+++ scripts/taxonomy/create_label_hierarchy.py
@@ -9,17 +9,17 @@ L = logging.getLogger("")
 
 MAPPING = {
     "http://www.w3.org/2004/02/skos/core": {
-        "uri":"http://www.w3.org/2009/08/skos-reference/skos.rdf",
-        "format":"xml"
-    } 
+        "uri": "http://www.w3.org/2009/08/skos-reference/skos.rdf",
+        "format": "xml"
+    }
 }
 
 
 NS = {
-    "rdf":"http://www.w3.org/1999/02/22-rdf-syntax-ns#", 
-    "rdfs":"http://www.w3.org/2000/01/rdf-schema#",
-    "owl":"http://www.w3.org/2002/07/owl#",
-    "skos": "http://www.w3.org/2004/02/skos/core#",   
+    "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+    "rdfs": "http://www.w3.org/2000/01/rdf-schema#",
+    "owl": "http://www.w3.org/2002/07/owl#",
+    "skos": "http://www.w3.org/2004/02/skos/core#",
     "obo": "http://purl.obolibrary.org/obo/",
     "geosciml": "http://resource.geosciml.org/classifier/cgi/lithology"
 }
@@ -36,38 +36,41 @@ PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 
 # https://rdflib.readthedocs.io/en/stable/intro_to_parsing.html
 # "turtle" is "text/turtle" media type
-REQUEST_FORMAT = "turtle" 
+REQUEST_FORMAT = "turtle"
+
+
 class OwlImporter:
     '''Implements a graph loader that recursively follows owl:imports.
     '''
-    
-    def __init__(self, mapping:dict={}, default_format:str="turtle"):
+
+    def __init__(self, mapping: dict = {}, default_format: str = "turtle"):
         self.mapping = mapping
         self.default_format = default_format
-        self._visited = []
-    
-    def resolve_uri(self, uri:typing.Union[str, rdflib.URIRef]):
-        '''Resolve URI to a graph.     
+        self._visited: typing.List[str] = []
+
+    def resolve_uri(self, uri: typing.Union[str, rdflib.URIRef]):
+        '''Resolve URI to a graph.
         '''
         uri = str(uri)
         g = rdflib.ConjunctiveGraph()
         if uri in self._visited:
             return g
-        # get URL from mapping, defaulting to the uri if a mapping entry is not available
-        url = self.mapping.get(uri,{}).get("uri", uri)
+        # get URL from mapping
+        # defaulting to the uri if a mapping entry is not available
+        url = self.mapping.get(uri, {}).get("uri", uri)
         # get the format for the document, defaulting to default_format
-        _format = self.mapping.get(uri,{}).get("format", self.default_format)
+        _format = self.mapping.get(uri, {}).get("format", self.default_format)
         try:
             g.parse(url, format=_format)
             self._visited.append(uri)
         except Exception as e:
             L.error(f"Unable to parse {uri} ({e})")
         return g
-        
+
     def load(
-        self, 
-        uri:str, 
-        follow_imports:bool=True
+        self,
+        uri: str,
+        follow_imports: bool = True
     ):
         '''Load a graph from uri. If follow_imports, then
         owl:imports are followed recursively.
@@ -84,13 +87,12 @@ class OwlImporter:
             L.error(f"Could not resolve {uri} ({e})")
 
 
-
 def loadGraph(url):
     owler = OwlImporter(mapping=MAPPING, default_format=REQUEST_FORMAT)
     g = owler.load(url)
     # bind some namespace prefixes for convenience
-    for k,v in NS.items():
-        g.bind(k,v)
+    for k, v in NS.items():
+        g.bind(k, v)
     return g
 
 
@@ -98,11 +100,14 @@ def skosT(term):
     # Create a skos:term
     return rdflib.URIRef(f"{NS['skos']}{term}")
 
+
 def rdfT(term):
     return rdflib.URIRef(f"{NS['rdf']}{term}")
 
+
 def rdfsT(term):
     return rdflib.URIRef(f"{NS['rdfs']}{term}")
+
 
 def listVocabularies(g):
     '''List the vocabularies in the provided graph
@@ -116,11 +121,11 @@ def listVocabularies(g):
     for r in qres:
         res.append(r[0])
     return res
-    
-    
+
+
 def getVocabularyRoot(g, v):
     """Get top concept of the vocabulary v in graph g
-    """    
+    """
     q = rdflib.plugins.sparql.prepareQuery(PFX + """SELECT ?s
     WHERE {
         ?s skos:topConceptOf ?vocabulary .
@@ -140,7 +145,7 @@ def getNarrowerV(g, v, r):
         ?s skos:inScheme ?vocabulary .
         ?s skos:broader ?parent .
     }""")
-    qres = g.query(q, initBindings={'vocabulary': v, 'parent':r})
+    qres = g.query(q, initBindings={'vocabulary': v, 'parent': r})
     res = []
     for row in qres:
         res.append(row[0])
@@ -154,7 +159,7 @@ def getNarrower(g, r):
     WHERE {
         ?s skos:broader ?parent .
     }""")
-    qres = g.query(q, initBindings={'parent':r})
+    qres = g.query(q, initBindings={'parent': r})
     res = []
     for row in qres:
         res.append(row[0])
@@ -168,85 +173,92 @@ def getObjects(g, s, p):
     WHERE {
         ?subject ?predicate ?o .
     }""")
-    qres = g.query(q, initBindings={'subject':s, 'predicate':p})
+    qres = g.query(q, initBindings={'subject': s, 'predicate': p})
     res = []
     for row in qres:
         res.append(row[0])
     return res
 
-def extractLanguage(s):
-    '''Return a label for the term s.
-    
-    First try for skos:prefLabel, then skos:label, and falling back to
-    the value at the end of the term URI
-    '''
-    try:
-        return 
-    except Exception as e:
-        pass
-    try:
-        label = getObjects(g, s, skosT("label"))
-        return label[0]
-    except Exception as e:
-        pass
-    parts = g.qname(s).split(":")
-    return parts[-1]
-
 
 def getLabel(g, s):
     '''Return a label for the term s.
-    
+
     First try for skos:prefLabel, then skos:label, and falling back to
     the value at the end of the term URI
     '''
     try:
         label = getObjects(g, s, skosT("prefLabel"))
         return label[0]
-    except Exception as e:
+    except Exception:
         pass
     try:
         label = getObjects(g, s, skosT("label"))
         return label[0]
-    except Exception as e:
+    except Exception:
         pass
     parts = g.qname(s).split(":")
     return parts[-1]
 
 
 def termTree(g, v, r, depth=0):
-    """Iterator yielding text rendering of narrower terms starting 
+    """Iterator yielding text rendering of narrower terms starting
     with term r of the vocabulary v within graph g
-    
+
     Each row is label + qname indented according to depth
     """
     label = getLabel(g, r)
     res = f"{'    '*depth}- {label} ({g.qname(r)})"
     yield res
     for term in getNarrower(g, r):
-        for res in termTree(g, v, term, depth=depth+1):
+        for res in termTree(g, v, term, depth=depth + 1):
             yield res
 
 
 def termTreeJSON(g, v, r, depth=0):
-    """Iterator yielding label object rendering of narrower terms starting 
+    """Iterator yielding label object rendering of narrower terms starting
     with term r of the vocabulary v within graph g
-    
-    """
-    label = getLabel(g,r)
-    lang = label.language if isinstance(label, rdflib.term.Literal) else "en" # defaults to en if language not provided
 
+    """
+    label = getLabel(g, r)
+
+    # defaults to en if language not provided
+    lang = label.language if isinstance(label, rdflib.term.Literal) else "en"
     label = str(label).strip()
     identifier = str(r).strip()
 
-    obj={}
+    obj = {}
     obj[identifier] = {}
     obj[identifier]["label"] = {lang: label}
-    obj[identifier]["children"]= []
+    obj[identifier]["children"] = []
 
     for term in getNarrower(g, r):
-        child_identifier,child_obj = termTreeJSON(g, v, term, depth=depth+1)
-        obj[identifier]["children"].append({child_identifier:child_obj})
-    return identifier, obj[identifier] # return key, value 
+        child_identifier, child_obj = termTreeJSON(g, v, term, depth=depth + 1)
+        obj[identifier]["children"].append({child_identifier: child_obj})
+    return identifier, obj[identifier]  # return key, value
+
+
+def getUrl(labelType):
+    """Return the url to fetch vocabulary"""
+    W3ID_BASE_URL = "https://w3id.org/isample/vocabulary/"
+    GITHUB_BASE_URL = "https://raw.githubusercontent.com/"
+    META_PARAM = "isamplesorg/metadata/develop/src/vocabularies/"
+
+    MATERIAL_URL = W3ID_BASE_URL + "material/0.9/materialsvocabulary"
+    SAMPLED_FEATURE_URL = GITHUB_BASE_URL + META_PARAM + "sampledFeature.ttl"
+    SPECIMEN_TYPE_URL = GITHUB_BASE_URL + META_PARAM + "specimenType.ttl"
+    OC_MATERIAL_URL = GITHUB_BASE_URL + META_PARAM + "OpenContextMaterial.ttl"
+
+    url = None
+    if labelType == "material":
+        url = MATERIAL_URL
+    elif labelType == "specimen":
+        url = SPECIMEN_TYPE_URL
+    elif labelType == "context":
+        url = SAMPLED_FEATURE_URL
+    elif labelType == "contextMaterial":
+        url = OC_MATERIAL_URL
+
+    return url
 
 
 def createJSON(url):
@@ -258,7 +270,7 @@ def createJSON(url):
     for vocab in vocabs:
         roots = getVocabularyRoot(g, vocab)
         for root in roots:
-            key, value= termTreeJSON(g, vocab, root)
+            key, value = termTreeJSON(g, vocab, root)
     data = {}
     data[key] = value
     json_data = json.dumps(data, indent=4)
@@ -266,22 +278,71 @@ def createJSON(url):
     return json_data
 
 
-def main():
-    material_url = "https://w3id.org/isample/vocabulary/material/0.9/materialsvocabulary"
-    sampledFeature_url = "https://raw.githubusercontent.com/isamplesorg/metadata/develop/src/vocabularies/sampledFeature.ttl"
-    openContextMaterial_url = "https://raw.githubusercontent.com/isamplesorg/metadata/develop/src/vocabularies/OpenContextMaterial.ttl"
-    specimenType_url = "https://raw.githubusercontent.com/isamplesorg/metadata/develop/src/vocabularies/specimenType.ttl"
+def updateMapping(g, v, r, parent, label_to_full):
+    """Recursively gets the full hierarchy of a label
+    that includes the broader terms
+    and adds that to the label_to_full mapping
+    e.g.) Frozen Water ==> Material>Any ice>Frozen water
+    """
 
-    material_hierarchy = createJSON(material_url)
-    sampledFeature_hierarchy = createJSON(sampledFeature_url)
-    openContextMaterial_hierarchy = createJSON(openContextMaterial_url)
-    specimenType_hierarchy = createJSON(specimenType_url)
+    label = getLabel(g, r)
+    label = str(label).strip()  # remove white space
+
+    full_hierarchy = parent + ">" + label if len(parent) > 0 else label
+    label_to_full[label] = full_hierarchy
+
+    for term in getNarrower(g, r):
+        # for each child node, update the mapping
+        updateMapping(g, v, term, full_hierarchy, label_to_full)
+
+
+def getHierarchyMapping(labelType):
+    """" For the label type, returns the hierarchy mapping
+    """
+    url = getUrl(labelType)
+
+    label_to_full = {}  # initialize hierarchy mapping
+    g = loadGraph(url)
+    vocabs = listVocabularies(g)
+    for vocab in vocabs:
+        print(f"Vocabulary: {vocab}")
+        roots = getVocabularyRoot(g, vocab)
+        for root in roots:
+            print(f"Root: {root}")
+            updateMapping(g, vocab, root, "", label_to_full)
+
+    # for key, value in label_to_full.items():
+    #     print(key,"\t",value)
+
+    return label_to_full
+
+
+def getFullLabel(label, mapping):
+    """Returns the full hierarchy of the given label"""
+    label = label[0]
+    if label in mapping:
+        return mapping[label]
+    else:
+        return "Not Provided"
+
+
+def main():
+
+    MATERIAL_URL = getUrl("material")
+    SAMPLED_FEATURE_URL = getUrl("context")
+    OC_MATERIAL_URL = getUrl("contextMaterial")
+    SPECIMEN_TYPE_URL = getUrl("specimen")
+
+    material_hierarchy = createJSON(MATERIAL_URL)
+    sampledFeature_hierarchy = createJSON(SAMPLED_FEATURE_URL)
+    openContextMaterial_hierarchy = createJSON(OC_MATERIAL_URL)
+    specimenType_hierarchy = createJSON(SPECIMEN_TYPE_URL)
 
     print(material_hierarchy)
     print(sampledFeature_hierarchy)
     print(openContextMaterial_hierarchy)
     print(specimenType_hierarchy)
-    
+
 
 if __name__ == "__main__":
     main()
---
+++ scripts/taxonomy/print_full_hierarchy.py
@@ -0,0 +1,74 @@
+import click
+import click_config_file
+from isamples_metadata import SESARTransformer
+
+import isb_lib
+from isb_lib.core import ThingRecordIterator
+from isb_web.sqlmodel_database import SQLModelDAO
+from create_label_hierarchy import getFullLabel, getHierarchyMapping
+
+
+@click.command()
+@click.option(
+    "-d",
+    "--db_url",
+    default=None,
+    help="Postgres database URL",
+)
+@click.option("-s", "--solr_url", default=None, help="Solr index URL")
+@click.option(
+    "-m",
+    "--max_records",
+    type=int,
+    default=1,
+    help="Maximum records to load, -1 for all",
+)
+@click.option(
+    "-v",
+    "--verbosity",
+    default="DEBUG",
+    help="Specify logging level",
+    show_default=True,
+)
+# You can specify the filename by doing --config <file> as the cmdline option
+@click_config_file.configuration_option()
+@click.pass_context
+def main(ctx, db_url: str, solr_url: str, max_records: int, verbosity: str):
+    isb_lib.core.things_main(ctx, db_url, solr_url, verbosity)
+    db_session = SQLModelDAO(db_url).get_session()
+    thing_iterator = ThingRecordIterator(
+        db_session,
+        authority_id="SESAR",
+        page_size=max_records
+    )
+    context_mapping = getHierarchyMapping("context")
+    material_mapping = getHierarchyMapping("material")
+    specimen_mapping = getHierarchyMapping("specimen")
+
+    for thing in thing_iterator.yieldRecordsByPage():
+        print(f"thing is {thing.id}")
+        transformed = SESARTransformer.SESARTransformer(
+            thing.resolved_content
+        ).transform()
+
+        context_label = transformed['hasContextCategory']
+        material_label = transformed['hasMaterialCategory']
+        specimen_label = transformed['hasSpecimenCategory']
+
+        full_context = [getFullLabel(context_label, context_mapping)]
+        full_material = [getFullLabel(material_label, material_mapping)]
+        full_specimen = [getFullLabel(specimen_label, specimen_mapping)]
+
+        print(f"context: {full_context} material: {full_material} "
+              f"specimen: {full_specimen}")
+
+        # print(f"context: {transformed['hasContextCategory']}
+        # material: {transformed['hasMaterialCategory']} "
+        #       f"specimen: {transformed['hasSpecimenCategory']}")
+    """
+    Exercise new machine driven classification
+    """
+
+
+if __name__ == "__main__":
+    main()
